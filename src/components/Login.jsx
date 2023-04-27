c47e7f965649662eabae87b9ff554aaa0815d8ca
harshal <harshalc562@gmail.com>
2023-04-28 01:35:03 +0530

    node modules

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Container  } from 'react-bootstrap'
 
 const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=c2607c2e688b4821baa81c302411813b&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+
 export default function Login() {
   return (
     <>
